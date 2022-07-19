9cd4e3a4b81cada996cf682bbb6a99eaf5fd3b98
Harini Jayawardhana <109010571+harijayk@users.noreply.github.com>
2022-07-19 09:47:41 +0530

    Update Q4B.scala

--- Q4B.scala
+++ Q4B.scala
@@ -8,8 +8,15 @@ object Question4b{
     def profit(ticketPrice:Int):Int = revenue(ticketPrice) - cost(ticketPrice);
 
     def main(args: Array[String]):Unit = {
-        println(profit(10));
-        println(profit(15));
-        println(profit(20));
+        println(profit(10));   //480
+        println(profit(15));   //940
+        println(profit(20));   //1200
+        println(profit(25));   //1260 highest profit
+        println(profit(30));   //1120
+        println(profit(35));   //780
+        println(profit(40));   //240
+        println(profit(45));   //-500
+        println(profit(50));   //-1440
+
     }
 }
